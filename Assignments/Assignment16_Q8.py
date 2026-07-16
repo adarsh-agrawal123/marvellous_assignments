e78a5d22ad39923de96f180c8b865c7a74a4582d
adarsh-agrawal123 <adarshagrawal343@gmail.com>
2026-07-16 11:23:16 +0530

    Assignment 16

---
+++ Assignments/Assignment16_Q8.py
@@ -0,0 +1,10 @@
+'''Write a program which accept number from user and print that number of "*" on screen.
+Input : 5                        Output : *     *     *     *     *'''
+
+
+def main():
+    no = int(input("Enter number : "))
+    print("*"*no)
+
+if __name__ == "__main__":
+    main()
