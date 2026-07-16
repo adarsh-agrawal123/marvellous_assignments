5423169949e8199523287a94982c948abb81df3f
adarsh-agrawal123 <adarshagrawal343@gmail.com>
2026-07-16 11:23:49 +0530

    Assignment 16

---
+++ Assignments/Assignment16_Q10.py
@@ -0,0 +1,10 @@
+'''Write a program which accept name from user and display length of its name.
+Input : Marvellous              Output : 10'''
+
+
+def main():
+    name = input()
+    print(len(name))
+
+if __name__ == "__main__":
+    main()
