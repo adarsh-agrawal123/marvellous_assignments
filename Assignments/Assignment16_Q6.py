659fb179d0b20a9d36546854f5d342c2b4708839
adarsh-agrawal123 <adarshagrawal343@gmail.com>
2026-07-16 11:22:44 +0530

    Assignment 16

---
+++ Assignments/Assignment16_Q6.py
@@ -0,0 +1,20 @@
+'''Write a program which accept number from user and check whether that number is positive 
+or negative or zero.
+Input : 11                      Output : Positive Number
+Input : -8                     Output : Negative Number
+Input : 0                       Output : Zero'''
+
+def ChkNum(no):
+    if(no > 0):
+        print("Positive Number")
+    elif(no < 0):
+        print("Negative Number")
+    else:
+        print("Zero")
+
+def main():
+    no = int(input("enter number : "))
+    ChkNum(no)
+
+if __name__ == "__main__":
+    main()
